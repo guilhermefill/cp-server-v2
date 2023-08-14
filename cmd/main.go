34ab24b1b6db5db3827df31865f97b15f532ed5f
Guilherme Fill <guilherme.fill@buddegroup.com>
2023-08-14 17:03:50 +0200

    graceful shut down

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -9,6 +14,10 @@ import (
 )
 
 func main() {
+
+	var wait time.Duration
+	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
+	flag.Parse()
 
 	r := mux.NewRouter()
 	routes.IndexHandler(r)
@@ -23,5 +32,29 @@ func main() {
 		WriteTimeout: 10 * time.Second,
 	}
 
-	srv.ListenAndServe()
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+	signal.Notify(c, os.Interrupt)
+
+	// Block until we receive our signal.
+	<-c
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	defer cancel()
+	// Doesn't block if no connections, but will otherwise wait
+	// until the timeout deadline.
+	srv.Shutdown(ctx)
+	// Optionally, you could run srv.Shutdown in a goroutine and block on
+	// <-ctx.Done() if your application should wait for other services
+	// to finalize based on context cancellation.
+	log.Println("shutting down")
+	os.Exit(0)
 }

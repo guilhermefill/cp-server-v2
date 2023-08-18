2ebd02a3bd8498af8fbb42b0d0b701aa4a092405
Guilherme Fill <guilherme.fill@buddegroup.com>
2023-08-18 11:59:32 +0200

    get app posts route

--- pkg/controllers/post-controller.go
+++ pkg/controllers/post-controller.go
@@ -1,16 +1,27 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/guilhermefill/cp-server-v2/pkg/models"
 	"github.com/guilhermefill/cp-server-v2/pkg/utils"
 )
 
 func GetAllPosts(w http.ResponseWriter, r *http.Request) {
 	utils.InfoLog(r)
-	fmt.Fprintf(w, "this is the posts route")
+	posts := models.GetAllPosts()
+	res, err := json.Marshal(posts)
+	if err != nil {
+		utils.ErrorLog(r)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
 
 func GetPostByID(w http.ResponseWriter, r *http.Request) {
--- pkg/models/posts.go
+++ pkg/models/posts.go
@@ -1,1 +1,51 @@
 package models
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Post struct {
+	ID             primitive.ObjectID `bson:"_id,omitempty"`
+	Title          string             `bson:"title,omitempty"`
+	Description    string             `bson:"description,omitempty"`
+	ImageUrl       string             `bson:"imageUrl,omitempty"`
+	PlaylistUrl    string             `bson:"playlistUrl,omitempty"`
+	Text           string             `bson:"text,omitempty"`
+	FavouriteCount int                `bson:"favouriteCount,omitempty"`
+	Creator        string             `bson:"creator,omitempty"`
+	Mood           []string           `bson:"mood,omitempty"`
+	Distance       int                `bson:"distance,omitempty"`
+	Length         int                `bson:"length,omitempty"`
+	Location       string             `bson:"location,omitempty"`
+}
+
+func GetAllPosts() *[]Post {
+	var posts []Post
+	coll := Client.Database("cp-server").Collection("posts")
+	findOptions := options.Find()
+
+	ctx := context.Background()
+
+	cursor, err := coll.Find(ctx, bson.M{}, findOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for cursor.Next(ctx) {
+		var result Post
+		err := cursor.Decode(&result)
+		if err != nil {
+			log.Fatal("cursor.Decode ERROR:", err)
+		}
+		posts = append(posts, result)
+	}
+
+	defer cursor.Close(ctx)
+
+	return &posts
+}
